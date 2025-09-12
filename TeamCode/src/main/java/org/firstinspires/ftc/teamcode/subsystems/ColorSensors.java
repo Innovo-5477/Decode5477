ea3385b249e37efd93203e52d652cb57e5ad8698
Rezivann <Rezivan@icloud.com>
2025-09-12 17:37:39 -0400

    Making command for left Color Sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ColorSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ColorSensors.java
@@ -7,29 +7,72 @@ import android.graphics.Color;
 import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.groups.ParallelGroup;
 import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.SubsystemGroup;
+import dev.nextftc.hardware.controllable.RunToVelocity;
 
 public class ColorSensors {
 
     private ColorSensors() {}
-    ColorSensor LeftSensor = hardwareMap.get(ColorSensor.class, "left_sensor");
-    ColorSensor MiddleSensor = hardwareMap.get(ColorSensor.class, "middle_sensor");
-    ColorSensor RightSensor = hardwareMap.get(ColorSensor.class, "right_sensor");
+    NormalizedColorSensor LeftSensor = hardwareMap.get(NormalizedColorSensor.class, "left_sensor");
+    NormalizedColorSensor MiddleSensor = hardwareMap.get(NormalizedColorSensor.class, "middle_sensor");
+    NormalizedColorSensor RightSensor = hardwareMap.get(NormalizedColorSensor.class, "right_sensor");
+    String outputLeft = "N";
+    public Command leftSensor = new LambdaCommand()
+            .setStart(() -> {
+                int [] minPurple = {0,0,0};
+                int [] maxPurple = {255, 255, 255};
 
-    float hsvValues[] = {0F, 0F, 0F};
-    final float values []= hsvValues;
+                int [] minGreen = {0,0,0};
+                int [] maxGreen = {255, 255, 255};
+                outputLeft = "N";
+            })
+            .setUpdate(() -> {
+                int [] minPurple = {0,0,0};
+                int [] maxPurple = {255, 255, 255};
 
-    final double SCALE_FACTOR = 255;
+                int [] minGreen = {0,0,0};
+                int [] maxGreen = {255, 255, 255};
+                // YOU NEED TO EDIT ALL OF THESE VALUES LATER
+                NormalizedRGBA colors = LeftSensor.getNormalizedColors();
+                double red = colors.red;
+                double green = colors.green;
+                double blue = colors.blue;
 
-    //final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+                boolean purpleRed = minPurple[0] < red && red < maxPurple[0];
+                boolean purpleGreen = minPurple[1] < green && green < maxPurple[1];
+                boolean purpleBlue = minPurple[2] < blue && blue < maxPurple[2];
+                boolean isPurple = purpleRed && purpleGreen && purpleBlue;
 
-    public String Left() {
+
+                boolean greenRed = minGreen[0] < red && red < maxGreen[0];
+                boolean greenGreen = minGreen[1] < green && green < maxGreen[1];
+                boolean greenBlue = minGreen[2] < blue && blue < maxGreen[2];
+                boolean isGreen = greenRed && greenGreen && greenBlue;
 
 
-    }
+                if (isPurple) {
+                    outputLeft = "P";
+                }
+                if(isGreen) {
+                    outputLeft = "G";
+                }
+            })
+            .setStop(interrupted -> {
 
+            })
+            .setIsDone(() -> true)
+            .requires()
+            .setInterruptible(true)
+            .setStop(interrupted -> {
+
+            })
+            .setIsDone(() -> true)
+            .requires()
+            .setInterruptible(true);
 }
