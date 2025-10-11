bf03b24795c37346873d82f14254e38d6c2101f8
dbalzac <david.l.balzac@gmail.com>
2025-10-11 13:33:31 -0400

    hopefully fixed intake pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -8,10 +8,12 @@ import org.firstinspires.ftc.robotcore.internal.hardware.android.GpioPin;
 import java.util.function.DoubleSupplier;
 
 import dev.nextftc.control.ControlSystem;
+import dev.nextftc.control.KineticState;
 import dev.nextftc.control.feedback.PIDCoefficients;
 import dev.nextftc.control.feedforward.BasicFeedforward;
 import dev.nextftc.control.feedforward.BasicFeedforwardParameters;
 import dev.nextftc.control.feedforward.FeedforwardElement;
+import dev.nextftc.core.commands.utility.InstantCommand;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.core.commands.Command;
 
@@ -23,7 +25,7 @@ import dev.nextftc.hardware.impl.MotorEx;
 public class Intake implements Subsystem {
     public static double target = 0;
     public static PIDCoefficients coefficients = new PIDCoefficients(0.0001, 0, 0);
-    public static BasicFeedforwardParameters ff = new BasicFeedforwardParameters(10);
+    public static BasicFeedforwardParameters ff = new BasicFeedforwardParameters(1, 1, 1);
     public static final Intake INSTANCE = new Intake();
     private Intake() { }
     private MotorEx intake_motor = new MotorEx("intake");
@@ -34,11 +36,23 @@ public class Intake implements Subsystem {
             .basicFF(ff)
             .build();
 
-    public Command intake = new RunToVelocity(controller, target).requires(this);
+    public Command intake = new InstantCommand(
+            () -> controller.setGoal(
+                    new KineticState(
+                            intake_motor.getCurrentPosition(),
+                            target
+                    )
+            )
+    );
 
     @Override
     public void periodic() {
-        intake_motor.setPower(controller.calculate(intake_motor.getState()));
+        intake_motor.setPower(controller.calculate(
+                new KineticState(
+                        intake_motor.getCurrentPosition(),
+                        intake_motor.getVelocity()
+                )
+        ));
         ActiveOpMode.telemetry().addData("Intake velocity: ", intake_motor.getVelocity());
         ActiveOpMode.telemetry().addData("Intake target: ", target);
         ActiveOpMode.telemetry().update();
